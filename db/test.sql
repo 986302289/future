7c3db02e96450b7665d517d8eb24636e46ac6e61
unknown <9863022859@qq.com>
2017-11-29 18:33:37 +0800

    1
    1

--- db/test.sql
+++ db/test.sql
@@ -1,29 +1,48 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost_3366
-Source Server Version : 50536
-Source Host           : localhost:3366
+Source Server         : TMS
+Source Server Version : 50019
+Source Host           : localhost:3306
 Source Database       : test
 
 Target Server Type    : MYSQL
-Target Server Version : 50536
+Target Server Version : 50019
 File Encoding         : 65001
 
-Date: 2016-07-28 12:15:31
+Date: 2017-11-29 18:31:25
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `f_zsd`
+-- ----------------------------
+DROP TABLE IF EXISTS `f_zsd`;
+CREATE TABLE `f_zsd` (
+  `zsdbs` int(11) NOT NULL auto_increment,
+  `zslx` varchar(11) NOT NULL,
+  `zsdbt` varchar(255) character set utf8 NOT NULL,
+  `zsdnr` varchar(8000) NOT NULL,
+  `czsj` datetime NOT NULL,
+  `cjsj` datetime NOT NULL,
+  PRIMARY KEY  (`zsdbs`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of f_zsd
+-- ----------------------------
+
 -- ----------------------------
 -- Table structure for `student`
 -- ----------------------------
 DROP TABLE IF EXISTS `student`;
 CREATE TABLE `student` (
-  `id` int(5) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) DEFAULT NULL,
-  `age` int(3) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
+  `id` int(5) NOT NULL auto_increment,
+  `name` varchar(20) default NULL,
+  `age` int(3) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of student
@@ -31,3 +50,6 @@ CREATE TABLE `student` (
 INSERT INTO `student` VALUES ('1', '张三', '23');
 INSERT INTO `student` VALUES ('2', '李四', '24');
 INSERT INTO `student` VALUES ('3', '王五', '25');
+INSERT INTO `student` VALUES ('32', '22', '22');
+INSERT INTO `student` VALUES ('33', '11', '11');
+INSERT INTO `student` VALUES ('34', '121', '121');
---
+++ src/com/bjpowernode/beans/F_ZSD.java
@@ -0,0 +1,58 @@
+package com.bjpowernode.beans;
+
+import java.sql.Timestamp;
+
+public class F_ZSD {
+	private Integer id;
+	private String zsdbs;
+	private String zslx;
+	private String zsdbt;
+	private String zsdnr;
+	private Timestamp czsj;
+	private Timestamp cjsj;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getZsdbs() {
+		return zsdbs;
+	}
+	public void setZsdbs(String zsdbs) {
+		this.zsdbs = zsdbs;
+	}
+	public String getZslx() {
+		return zslx;
+	}
+	public void setZslx(String zslx) {
+		this.zslx = zslx;
+	}
+	public String getZsdbt() {
+		return zsdbt;
+	}
+	public void setZsdbt(String zsdbt) {
+		this.zsdbt = zsdbt;
+	}
+	public String getZsdnr() {
+		return zsdnr;
+	}
+	public void setZsdnr(String zsdnr) {
+		this.zsdnr = zsdnr;
+	}
+	public Timestamp getCzsj() {
+		return czsj;
+	}
+	public void setCzsj(Timestamp czsj) {
+		this.czsj = czsj;
+	}
+	public Timestamp getCjsj() {
+		return cjsj;
+	}
+	public void setCjsj(Timestamp cjsj) {
+		this.cjsj = cjsj;
+	}
+
+
+}
